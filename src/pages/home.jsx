729ad44e79390b8ceae5f4a6700bf5163e247eaf
Ravi Flynaut <ravik@flynaut.com>
2023-07-21 14:53:15 +0530

    added script and jquery

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,14 +1,13 @@
 import PricingFeatures from "@/components/pricingFeatures/PricingFeatures";
 import React from "react"; 
-import WelcomeAboard from "@/components/welcome-aboard";
 
 
 export default function Home() {
   return (
     <div>
-      {/* <PricingFeatures /> */}
+      <PricingFeatures />
 
-    <WelcomeAboard />
+
 
 
     </div>
