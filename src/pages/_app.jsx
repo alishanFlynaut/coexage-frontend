3a2015aefd5e9cfcdfc7c8f333a9799815201e27
Ravi Flynaut <ravik@flynaut.com>
2023-07-21 12:24:38 +0530

    added the css,jquery,welcome page

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,11 @@
 import "@/styles/globals.css";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return   <>
+   <Script src="https://code.jquery.com/jquery-3.6.0.min.js" strategy="beforeInteractive" />
+    <Component {...pageProps} />;
+  
+  </>
+   
 }
