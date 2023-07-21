81449b8e75db2975cce8323781333e574fa7f0ef
Ravi Flynaut <ravik@flynaut.com>
2023-07-21 11:35:20 +0530

    modiefied the style.css,added images, for welcome aboard.html

---
+++ src/pages/home.jsx
@@ -0,0 +1,16 @@
+import PricingFeatures from "@/components/pricingFeatures/PricingFeatures";
+import React from "react"; 
+
+
+
+export default function Home() {
+  return (
+    <div>
+      <PricingFeatures />
+
+
+
+
+    </div>
+  );
+}
